5abfd6fb9e782eee74eb9811e22b4048503c7a5e
JXS <113408314+Jishan76@users.noreply.github.com>
2024-03-18 15:55:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const ytSearch = require('yt-search');
 const ytdl = require('ytdl-core');
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/video', async (req, res) => {
+  const query = req.query.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Missing query parameter' });
+  }
+
+  try {
+    // Search for videos using yt-search
+    const { videos } = await ytSearch(query);
+
+    if (!videos.length) {
+      return res.status(404).json({ error: 'No videos found' });
+    }
+
+    // Get the first video
+    const firstVideo = videos[0];
+
+    // Get video details
+    const videoInfo = await ytdl.getInfo(firstVideo.url);
+
+    // Get the highest quality video format
+    const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestvideo' });
+    if (!videoFormat) {
+      return res.status(404).json({ error: 'No video found' });
+    }
+
+    const videoUrl = videoFormat.url;
+    const videoTitle = videoInfo.videoDetails.title;
+
+    const responseJson = {
+      title: videoTitle,
+      url: videoUrl
+    };
+
+    res.json(responseJson);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.get('/audio', async (req, res) => {
   const query = req.query.query;
@@ -31,18 +74,15 @@ app.get('/audio', async (req, res) => {
       return res.status(404).json({ error: 'No audio found' });
     }
 
+    const audioUrl = audioFormat.url;
     const audioTitle = videoInfo.videoDetails.title;
 
-    // Set headers for file download
-    res.set({
-      'Content-Disposition': `attachment; filename="${audioTitle}.mp3"`,
-      'Content-Type': 'audio/mpeg',
-    });
+    const responseJson = {
+      title: audioTitle,
+      url: audioUrl
+    };
 
-    // Pipe the audio stream to response for download
-    ytdl(firstVideo.url, { format: audioFormat })
-      .pipe(res);
-
+    res.json(responseJson);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
